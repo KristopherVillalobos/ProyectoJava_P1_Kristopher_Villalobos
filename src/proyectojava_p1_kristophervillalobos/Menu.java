e9ef7f5b91c6b77956e883fb03a5dc9aee675f6f
KristopherVillalobos <kdvillalobos@unitec.edu>
2024-06-10 23:31:57 -0600

    Menu

---
+++ src/proyectojava_p1_kristophervillalobos/Menu.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectojava_p1_kristophervillalobos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author krist
+ */
+public class Menu {
+    
+    class Plato {
+
+        private String nombre;
+        private String categoria;
+        private double precio;
+        private boolean disponible;
+
+        public Plato(String nombre, String categoria, double precio, boolean disponible) {
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.precio = precio;
+            this.disponible = disponible;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getCategoria() {
+            return categoria;
+        }
+
+        public double getPrecio() {
+            return precio;
+        }
+
+        public boolean isDisponible() {
+            return disponible;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void setCategoria(String categoria) {
+            this.categoria = categoria;
+        }
+
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+
+        public void setDisponible(boolean disponible) {
+            this.disponible = disponible;
+        }
+    }
+
+    private List<Plato> platos;
+
+    public Menu() {
+        this.platos = new ArrayList<>();
+    }
+
+    public void agregarPlato(Plato plato) {
+        platos.add(plato);
+    }
+
+    public void eliminarPlato(String nombre) {
+        platos.removeIf(plato -> plato.getNombre().equals(nombre));
+    }
+
+    public void actualizarPlato(String nombre, Plato nuevoPlato) {
+        for (Plato plato : platos) {
+            if (plato.getNombre().equals(nombre)) {
+                plato.setNombre(nuevoPlato.getNombre());
+                plato.setCategoria(nuevoPlato.getCategoria());
+                plato.setPrecio(nuevoPlato.getPrecio());
+                plato.setDisponible(nuevoPlato.isDisponible());
+            }
+        }
+    }
+
+    public void listarPlatos() {
+        for (Plato plato : platos) {
+            System.out.println("Nombre: " + plato.getNombre() + ", Categoría: " + plato.getCategoria() + ", Precio: " + plato.getPrecio() + ", Disponible: " + plato.isDisponible());
+        }
+    }
+}
--- src/proyectojava_p1_kristophervillalobos/ProyectoJava_P1_KristopherVillalobos.java
+++ src/proyectojava_p1_kristophervillalobos/ProyectoJava_P1_KristopherVillalobos.java
@@ -18,6 +18,8 @@ public class ProyectoJava_P1_KristopherVillalobos {
     public static void main(String[] args) {
         // TODO code application logic here
 
+        Menu platos = new Menu();
+        
         Scanner scanner = new Scanner(System.in);
         
         int opc;
@@ -36,6 +38,7 @@ public class ProyectoJava_P1_KristopherVillalobos {
             
             switch(opc){
                 case 1:
+                    menuplatos(platos, scanner);
                     break;
                 case 2:
                     break;
@@ -53,5 +56,64 @@ public class ProyectoJava_P1_KristopherVillalobos {
             }
         }
         while( opc!= 6);
+    }
+    
+    private static void menuplatos(Menu platos, Scanner scanner) {
+        
+        int menuopc;
+        
+        Scanner lea = new Scanner(System.in);
+        
+        do {
+            System.out.println("------------MENU------------");
+            System.out.println("1. Agregar Plato");
+            System.out.println("2. Eliminar Plato");
+            System.out.println("3. Actualizar Plato");
+            System.out.println("4. Listar Platos");
+            System.out.println("5. Volver al Menu Principal");
+            System.out.print("Ingresa una Opcion: ");
+            menuopc = scanner.nextInt();
+
+            switch (menuopc) {
+                case 1:
+                    System.out.print("Introduce el nombre del plato: ");
+                    String nombre = lea.nextLine();
+                    System.out.print("Introduce la categoría: ");
+                    String categoria = lea.nextLine();
+                    System.out.print("Introduce el precio: ");
+                    double precio = lea.nextDouble();
+                    System.out.print("Disponibilidad: ");
+                    boolean disponible = lea.nextBoolean();
+                    Menu.Plato plato = platos.new Plato(nombre, categoria, precio, disponible);
+                    platos.agregarPlato(plato);
+                    break;
+                case 2:
+                    System.out.print("Introduce el nombre del plato a eliminar: ");
+                    String nombrePlatoEliminar = lea.nextLine();
+                    platos.eliminarPlato(nombrePlatoEliminar);
+                    break;
+                case 3:
+                    System.out.print("Introduce el nombre del nuevo plato: ");
+                    String nombrePlatoActualizar = lea.nextLine();
+                    System.out.print("Introduce el nuevo nombre: ");
+                    String nuevoNombre = lea.nextLine();
+                    System.out.print("Introduce la nueva categoría: ");
+                    String nuevaCategoria = lea.nextLine();
+                    System.out.print("Introduce el nuevo precio: ");
+                    double nuevoPrecio = lea.nextDouble();
+                    System.out.print("Disponibilidad: ");
+                    boolean nuevaDisponibilidad = lea.nextBoolean();
+                    Menu.Plato nuevoPlato = platos.new Plato(nuevoNombre, nuevaCategoria, nuevoPrecio, nuevaDisponibilidad);
+                    platos.actualizarPlato(nombrePlatoActualizar, nuevoPlato);
+                    break;
+                case 4:
+                    platos.listarPlatos();
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Opcion no Valida. Intenta de nuevo.");
+            }
+        } while (menuopc != 5);
     }
 }
